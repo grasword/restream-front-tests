1f99aef332760ec9af33a63abe417dbeecbe71b9
Oleg <Grasword6@gmail.com>
2020-02-29 04:44:43 +0200

    edited text property in testData

--- testData/testData.js
+++ testData/testData.js
@@ -5,14 +5,15 @@ export const productsBtn = [
     title:
       "<title>Reach a Wider Audience with Multistreaming | Restream</title>",
     text:
-      "<div>Reach a wider audience by streaming to multiple platforms simultaneously.</div>"
+      "Reach a wider audience by streaming to multiple platforms simultaneously.</div>",
     visible: false
   },
   {
     name: "Chat",
     link: "https://restream.io/chat",
     title: "<title>Cross-Platform Chat | Restream</title>",
-    text: "<h1>Engage viewers in a multi chat</h1>"
+    text:
+      "You don’t want offensive words or repetitive bots in your livestream.<span>",
     visible: false
   },
   {
@@ -20,38 +21,39 @@ export const productsBtn = [
     link: "https://restream.io/scheduler",
     title:
       "<title>Schedule and Stream Pre-Recorded Videos Live | Restream</title>",
-    text: "<h1>Stream your recorded videos live</h1>"
+    text: "Stream your recorded videos live</h1>",
     visible: false
   },
   {
     name: "Analytics",
     link: "https://restream.io/analytics",
     title: "<title>Analyze Your Stream Performance | Restream</title>",
-    text: "<h1>Measure your success</h1>"
-  }
+    text:
+      "Get insights on your live stream performance across multiple platforms – on a single screen.</div>",
     visible: false
+  },
 
-export const menuBtns = [
+  // Menu Butns
   {
     name: "About",
     link: "https://restream.io/about",
     title: "<title>About Us | Restream</title>",
     text:
-      "<h1>Restream allows you to broadcast live video to 30+ social networks at the same time.</h1>"
+      "Restream allows you to broadcast live video to 30+ social networks at the same time.</h1>",
     visible: true
   },
   {
     name: "Customers",
     link: "https://restream.io/customers",
     title: "<title>Our Customers | Restream</title>",
-    text: "<h1>Over 2,000,000 streamers worldwide choose Restream</h1>"
+    text: "Over 2,000,000 streamers worldwide choose Restream</h1>",
     visible: true
   },
   {
     name: "Pricing",
     link: "https://restream.io/pricing",
     title: "<title>Plans & Pricing | Restream</title>",
-    text: "<h1>Pick the right Restream</h1>"
+    text: "Pick the right Restream</h1>",
     visible: true
   }
 ];
