b1fc0781076274c15ff9cfa047ff2a60334e2c38
Oleg <Grasword6@gmail.com>
2020-02-29 02:47:21 +0200

    create testData file

---
+++ testData/testData.js
@@ -0,0 +1,51 @@
+export const productsBtn = [
+  {
+    name: "Multistreaming",
+    link: "https://restream.io/multistreaming",
+    title:
+      "<title>Reach a Wider Audience with Multistreaming | Restream</title>",
+    text:
+      "<div>Reach a wider audience by streaming to multiple platforms simultaneously.</div>"
+  },
+  {
+    name: "Chat",
+    link: "https://restream.io/chat",
+    title: "<title>Cross-Platform Chat | Restream</title>",
+    text: "<h1>Engage viewers in a multi chat</h1>"
+  },
+  {
+    name: "Scheduler",
+    link: "https://restream.io/scheduler",
+    title:
+      "<title>Schedule and Stream Pre-Recorded Videos Live | Restream</title>",
+    text: "<h1>Stream your recorded videos live</h1>"
+  },
+  {
+    name: "Analytics",
+    link: "https://restream.io/analytics",
+    title: "<title>Analyze Your Stream Performance | Restream</title>",
+    text: "<h1>Measure your success</h1>"
+  }
+];
+
+export const menuBtns = [
+  {
+    name: "About",
+    link: "https://restream.io/about",
+    title: "<title>About Us | Restream</title>",
+    text:
+      "<h1>Restream allows you to broadcast live video to 30+ social networks at the same time.</h1>"
+  },
+  {
+    name: "Customers",
+    link: "https://restream.io/customers",
+    title: "<title>Our Customers | Restream</title>",
+    text: "<h1>Over 2,000,000 streamers worldwide choose Restream</h1>"
+  },
+  {
+    name: "Pricing",
+    link: "https://restream.io/pricing",
+    title: "<title>Plans & Pricing | Restream</title>",
+    text: "<h1>Pick the right Restream</h1>"
+  }
+];
