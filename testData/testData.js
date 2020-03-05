20be570da34197bc23c61530e27244c679300a03
Oleg <Grasword6@gmail.com>
2020-03-05 16:17:52 +0200

    changed naming for testData (href, isVisible)

--- testData/testData.js
+++ testData/testData.js
@@ -1,59 +1,59 @@
-export const menuBtns = [
+export const headerLinks = [
   {
     name: "Multistreaming",
-    link: "https://restream.io/multistreaming",
+    href: "https://restream.io/multistreaming",
     title:
       "<title>Reach a Wider Audience with Multistreaming | Restream</title>",
     text:
       "Reach a wider audience by streaming to multiple platforms simultaneously.</div>",
-    visible: false
+    isVisible: false
   },
   {
     name: "Chat",
-    link: "https://restream.io/chat",
+    href: "https://restream.io/chat",
     title: "<title>Cross-Platform Chat | Restream</title>",
     text:
       "You don’t want offensive words or repetitive bots in your livestream.<span>",
-    visible: false
+    isVisible: false
   },
   {
     name: "Scheduler",
-    link: "https://restream.io/scheduler",
+    href: "https://restream.io/scheduler",
     title:
       "<title>Schedule and Stream Pre-Recorded Videos Live | Restream</title>",
     text: "Stream your recorded videos live</h1>",
-    visible: false
+    isVisible: false
   },
   {
     name: "Analytics",
-    link: "https://restream.io/analytics",
+    href: "https://restream.io/analytics",
     title: "<title>Analyze Your Stream Performance | Restream</title>",
     text:
       "Get insights on your live stream performance across multiple platforms – on a single screen.</div>",
-    visible: false
+    isVisible: false
   },
 
   // Menu Butns
   {
     name: "About",
-    link: "https://restream.io/about",
+    href: "https://restream.io/about",
     title: "<title>About Us | Restream</title>",
     text:
       "Restream allows you to broadcast live video to 30+ social networks at the same time.</h1>",
-    visible: true
+    isVisible: true
   },
   {
     name: "Customers",
-    link: "https://restream.io/customers",
+    href: "https://restream.io/customers",
     title: "<title>Our Customers | Restream</title>",
     text: "Over 2,000,000 streamers worldwide choose Restream</h1>",
-    visible: true
+    isVisible: true
   },
   {
     name: "Pricing",
-    link: "https://restream.io/pricing",
+    href: "https://restream.io/pricing",
     title: "<title>Plans & Pricing | Restream</title>",
     text: "Pick the right Restream</h1>",
-    visible: true
+    isVisible: true
   }
 ];
