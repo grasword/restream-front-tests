f8822d23bd7a57fcbf19011efbc1c49bc5d6cb7b
Oleg <Grasword6@gmail.com>
2020-03-27 18:10:58 +0200

    added test data for footer links

--- testData/testData.js
+++ testData/testData.js
@@ -33,7 +33,7 @@ export const headerLinks = [
     isVisible: false
   },
 
-  // Menu Butns
+  // Menu buttons
   {
     name: "About",
     href: "https://restream.io/about",
@@ -57,3 +57,52 @@ export const headerLinks = [
     isVisible: true
   }
 ];
+
+export const footerLinks = [
+  {
+    name: "Advertising for brands",
+    href: "https://restream.io/rads",
+    title: "<title>Let your game be discovered | Restream Ads</title>",
+    text: "Instantly activate 2 Million live streamers!</div>"
+  },
+  {
+    name: "Blog",
+    href: "https://restream.io/blog/",
+    title:
+      "<title>Multistreaming Tips, Guides, and Updates – Restream Blog</title>",
+    text: "About</a>"
+  },
+  {
+    name: "Careers",
+    href: "https://restream.io/careers",
+    title: "<title>Careers | Restream</title>",
+    text:
+      "By improving the distribution and monetization of Live Video Content.</div>"
+  },
+  {
+    name: "Help center",
+    href: "https://support.restream.io/en",
+    title: "<title>Restream Help Center</title>",
+    text: "here to help!</h1>"
+  },
+  {
+    name: "Privacy policy",
+    href: "https://restream.io/faq/privacy-policy",
+    title: "<title>Privacy Policy – Restream</title>",
+    text: "Privacy policy</h1>"
+  },
+  {
+    name: "Terms",
+    href: "https://restream.io/faq/terms-of-use",
+    title: "<title>Terms of Use – Restream</title>",
+    text: "Terms of use</h1>"
+  },
+  {
+    name: "Developers",
+    href: "https://developers.restream.io/",
+    title: "<title>Developer Portal - Restream</title>",
+    text: "These developer pages are designed for those interested"
+  }
+];
+
+
