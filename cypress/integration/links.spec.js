499108f9a2c6385a3a71678be1d1191b2c64df6c
Oleg <Grasword6@gmail.com>
2020-03-27 16:27:36 +0200

    refactor links.spec (divided between two logical functions)

--- cypress/integration/links.spec.js
+++ cypress/integration/links.spec.js
@@ -5,30 +5,43 @@ context("Header links", () => {
     cy.visit("/");
   });
 
-  headerLinks.forEach(anchor => {
-    describe(`testing the target=/${anchor.name} link`, () => {
-      it("verify the href", () => {
+  describe("testing links", () => {
+    headerLinks.forEach(anchor => {
+      it(`'${anchor.name}' have valid href`, () => {
         cy.get(".menu")
           .contains(anchor.name)
           .should($a => {
-            expect($a).to.be[anchor.isVisible ? "visible" : "hidden"];
-
             expect($a).to.have.prop("href");
             expect($a.prop("href")).eq(anchor.href);
           });
       });
 
-      it("request without visiting", () => {
-        cy.contains(anchor.name).then($a => {
-          const href = $a.prop("href");
-
-          cy.request(href).then(resp => {
-            expect(resp.status).to.eq(200);
-
-            expect(resp.body).to.include(anchor.title);
-            expect(resp.body).to.include(anchor.text);
-          });
+      if (!anchor.isVisible) {
+        it(`'${anchor.name}' is hidden`, () => {
+          cy.get(".menu")
+            .contains(anchor.name)
+            .should($a => {
+              expect($a).to.be.hidden;
+            });
         });
+      }
+    });
+  });
+
+  describe("request without visiting", () => {
+    headerLinks.forEach(anchor => {
+      it(`'${anchor.name}' have valid response`, () => {
+        cy.get(".menu")
+          .contains(anchor.name)
+          .then($a => {
+            const href = $a.prop("href");
+
+            cy.request(anchor.href).then(resp => {
+              expect(resp.status).to.eq(200);
+              expect(resp.body).to.include(anchor.title);
+              expect(resp.body).to.include(anchor.text);
+            });
+          });
       });
     });
   });
