05029c57b6ba9f11c9e1f63335ccfff6bfcd622e
Oleg <Grasword6@gmail.com>
2020-04-26 20:04:51 +0300

    updeted test data

--- testData/testData.js
+++ testData/testData.js
@@ -5,32 +5,32 @@ export const headerLinks = [
     title:
       "<title>Reach a Wider Audience with Multistreaming | Restream</title>",
     text:
-      "Reach a wider audience by streaming to multiple platforms at the same time.</div>",
-    isVisible: false
+      "Reach a wider audience by streaming to multiple platforms at the same time.",
+    isVisible: false,
   },
   {
     name: "Chat",
     href: "https://restream.io/chat",
     title: "<title>Cross-Platform Chat | Restream</title>",
     text:
-      "You don’t want offensive words or repetitive bots in your livestream.<span>",
-    isVisible: false
+      "You don’t want offensive words or repetitive bots in your livestream.",
+    isVisible: false,
   },
   {
     name: "Scheduler",
     href: "https://restream.io/scheduler",
     title:
       "<title>Schedule and Stream Pre-Recorded Videos Live | Restream</title>",
-    text: "Stream your recorded videos live</h1>",
-    isVisible: false
+    text: "Stream your recorded videos live",
+    isVisible: false,
   },
   {
     name: "Analytics",
     href: "https://restream.io/analytics",
     title: "<title>Analyze Your Stream Performance | Restream</title>",
     text:
-      "Get insights on your live stream performance across multiple platforms – on a single screen.</div>",
-    isVisible: false
+      "Get insights on your live stream performance across multiple platforms – on a single screen.",
+    isVisible: false,
   },
 
   // Menu buttons
@@ -39,23 +39,23 @@ export const headerLinks = [
     href: "https://restream.io/about",
     title: "<title>About Us | Restream</title>",
     text:
-      "Restream allows you to broadcast live video to 30+ social networks at the same time.</h1>",
-    isVisible: true
+      "Restream allows you to broadcast live video to 30+ social networks at the same time.",
+    isVisible: true,
   },
   {
     name: "Customers",
     href: "https://restream.io/customers",
     title: "<title>Our Customers | Restream</title>",
-    text: "Over 2,000,000 streamers worldwide choose Restream</h1>",
-    isVisible: true
+    text: "Over 2,000,000 streamers worldwide choose Restream",
+    isVisible: true,
   },
   {
     name: "Pricing",
     href: "https://restream.io/pricing",
     title: "<title>Plans & Pricing | Restream</title>",
-    text: "Pick the right Restream</h1>",
-    isVisible: true
-  }
+    text: "Pick the right Restream",
+    isVisible: true,
+  },
 ];
 
 export const footerLinks = [
@@ -63,46 +63,38 @@ export const footerLinks = [
     name: "Advertising for brands",
     href: "https://restream.io/rads",
     title: "<title>Let your game be discovered | Restream Ads</title>",
-    text: "Instantly activate 2 Million live streamers!</div>"
+    text: "Instantly activate 2 Million live streamers!",
   },
   {
     name: "Blog",
     href: "https://restream.io/blog/",
     title:
       "<title>Multistreaming Tips, Guides, and Updates – Restream Blog</title>",
-    text: "About</a>"
+    text: "About",
   },
   {
     name: "Careers",
     href: "https://restream.io/careers",
     title: "<title>Careers | Restream</title>",
     text:
-      "By improving the distribution and monetization of Live Video Content.</div>"
+      "By improving the distribution and monetization of Live Video Content.",
   },
   {
     name: "Help center",
     href: "https://support.restream.io/en",
     title: "<title>Restream Help Center</title>",
-    text: "here to help!</h1>"
+    text: "here to help!",
   },
   {
-    name: "Privacy policy",
-    href: "https://restream.io/faq/privacy-policy",
-    title: "<title>Privacy Policy – Restream</title>",
-    text: "Privacy policy</h1>"
-  },
-  {
-    name: "Terms",
+    name: "Terms & Privacy",
     href: "https://restream.io/faq/terms-of-use",
     title: "<title>Terms of Use – Restream</title>",
-    text: "Terms of use</h1>"
+    text: "Terms of use",
   },
   {
     name: "Developers",
     href: "https://developers.restream.io/",
     title: "<title>Developer Portal - Restream</title>",
-    text: "These developer pages are designed for those interested"
-  }
+    text: "These developer pages are designed for those interested",
+  },
 ];
-
-
