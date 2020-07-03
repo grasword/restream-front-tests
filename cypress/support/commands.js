60cb539173f1220758f8583649b1233263c8bae6
Oleg <Grasword6@gmail.com>
2020-07-03 16:38:03 +0300

    [conf] add type for `cypress-commands` in `tsconfig.json`

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -24,6 +24,46 @@
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
 
+Cypress.Commands.add('upload_file', (fileName, type, selector) => {
+  cy.get(selector).then((subject) => {
+    cy.fixture(fileName, 'base64').then((content) => {
+      const el = subject[0]
+      const blob = b64toBlob(content, type)
+      cy.window().then((win) => {
+        const testFile = new win.File([blob], fileName, { type })
+        const dataTransfer = new DataTransfer()
+
+        dataTransfer.items.add(testFile)
+        el.files = dataTransfer.files
+      })
+    })
+  })
+
+  cy.get(selector).trigger('change', { force: true })
+})
+
+function b64toBlob (b64Data, contentType = '', sliceSize = 512) {
+  const byteCharacters = atob(b64Data)
+  const byteArrays = []
+
+  for (let offset = 0; offset < byteCharacters.length; offset += sliceSize) {
+    const slice = byteCharacters.slice(offset, offset + sliceSize)
+
+    const byteNumbers = new Array(slice.length)
+    for (let i = 0; i < slice.length; i++) {
+      byteNumbers[i] = slice.charCodeAt(i)
+    }
+
+    const byteArray = new Uint8Array(byteNumbers)
+
+    byteArrays.push(byteArray)
+  }
+
+  const blob = new Blob(byteArrays, { type: contentType })
+  blob.lastModifiedDate = new Date()
+  return blob
+}
+
 Cypress.Commands.add('login', (email, password) => {
   cy.request({
     method: 'POST',
